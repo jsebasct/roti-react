e78b7338d15b5ee5bfd8d80ce385d267e42c0c3a
scruz <sebastian.cruz@bibamgroup.com>
2019-05-16 12:05:49 -0300

    feat: pickle

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+
+//esto no funciona porq no esta installado
+// hay que hacer npm 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const amIAPickle = () => true;
 const getMyPickle = () => <div> Soy un pickle rick </div>
 
 class Fruit extends React.Component {
   render() {
-    amIAPickle() ? getMyPickle() : <div />
+    return amIAPickle() ? getMyPickle() : <div />
   }
 }
 
-export default Fruit
+
+ReactDOM.render( <Fruit/> , document.getElementById('myApp'));
+
+//export default Fruit
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const webpack = require("webpack");
+module.exports = {
+  entry: "./index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env", "@babel/preset-react"] }
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  }
+};
